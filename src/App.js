821d92b2fb17619a6f938a0ec2f40b9ae8d29094
jakubrejch <jrejch@citizengo.net>
2022-12-14 19:33:50 +0100

    change image url for prod

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { useEffect, useState } from 'react';
 import Home from './pages/Home';
 import Zak from './pages/Zak';
 import Hela from './pages/Hela';
@@ -9,13 +8,13 @@ import Memory from './components/Memory';
 import Puzzle from './components/Puzzle';
 
 const App = () => {
-  let path = window.location.hostname === 'localhost' ? '/' : '/some_web';
+  let path = window.location.hostname === 'localhost' ? '/' : "";
   return (
     <>
       <Router basename={path}>
         <Header />
         <Routes>
-          <Route exact path="/" element={<Home />} />
+          <Route exact path={path} element={<Home />} />
           <Route path="/zaki" element={<Zak path={path} />} />
           <Route path="/hela" element={<Hela />} />
           <Route path="/ida" element={<Ida />} />
